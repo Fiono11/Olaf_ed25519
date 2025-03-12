b64ca010fe4bcccebbb884fb92604c24a9a2af14
Fiono <ruipedromorais11@gmail.com>
2025-03-12 14:30:35 +0000

    Fix

--- src/simplpedpop/mod.rs
+++ src/simplpedpop/mod.rs
@@ -75,9 +75,7 @@ impl SigningKeypair {
 
             let recipient: VerifyingKey = recipients[i as usize];
 
-            let scalar = ExpandedSecretKey::from(&ephemeral_key.secret_key).scalar;
-
-            let key_exchange = scalar
+            let key_exchange = ephemeral_key.to_scalar()
                 * CompressedEdwardsY::from_slice(recipient.as_bytes())
                     .unwrap()
                     .decompress()
